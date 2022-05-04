85a04bac2d31afcafb70aa3a03aa1f765d5268f7
Samuel <54447830+Ausaci@users.noreply.github.com>
2022-05-04 14:42:47 +0800

    fix static compile & add dynamic compile

--- attic/world/buildmoon.sh
+++ attic/world/buildmoon.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-c++ -std=c++11 -I../.. -I.. -g -o mkmoonworld ../../node/C25519.cpp ../../node/Salsa20.cpp ../../node/SHA512.cpp ../../node/Identity.cpp ../../node/Utils.cpp ../../node/InetAddress.cpp ../../osdep/OSUtils.cpp mkmoonworld.cpp -lm -static
+# static compile
+# fix the error "undefined reference to 'pthread_create','pthread_mutex_unlock',etc." of linux os, in which the "pthread" function is missed, fixing by adding "-lpthread" parameter. # https://blog.csdn.net/qq_35056292/article/details/70182484
+c++ -std=c++11 -I../.. -I.. -g -o mkmoonworld-static ../../node/C25519.cpp ../../node/Salsa20.cpp ../../node/SHA512.cpp ../../node/Identity.cpp ../../node/Utils.cpp ../../node/InetAddress.cpp ../../osdep/OSUtils.cpp mkmoonworld.cpp -lm -lpthread -static
+
+# dynamic compile
+# c++ -std=c++11 -I../.. -I.. -g -o mkmoonworld-dynamic ../../node/C25519.cpp ../../node/Salsa20.cpp ../../node/SHA512.cpp ../../node/Identity.cpp ../../node/Utils.cpp ../../node/InetAddress.cpp ../../osdep/OSUtils.cpp mkmoonworld.cpp -lm
